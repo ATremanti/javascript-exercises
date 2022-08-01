bd5f1b7f569f55c8fb2e9737f658b1a6d5c5af3c
ATremanti <arthurtremanti@gmail.com>
2022-08-01 11:01:56 -0300

    exercises 8 to 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    if (index < 0) {
+        return "OOPS"
+    } else {
+        let first = 0
+        let next = 1
 
+        for (i = 1; i < index; i++) {
+            let temp = next
+            next = first + next
+            first = temp
+        }
+        return next
+    }
 };
 
 // Do not edit below this line
