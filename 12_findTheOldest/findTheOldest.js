230a8475810cb304ee5e7f1ceaa82660168c1d4b
ATremanti <arthurtremanti@gmail.com>
2022-08-02 09:05:00 -0300

    Exercises 11 and 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    return arr.reduce((personA, personB) => {
+        const ageA = getAge(personA.yearOfDeath, personA.yearOfBirth)
+        const ageB = getAge(personB.yearOfDeath, personB.yearOfBirth)
+        return ageA > ageB ? personA : personB
+    })
 
 };
+
+
+const getAge = (deathYear, birthYear) => {
+    if (!deathYear) {
+        deathYear = new Date().getFullYear()
+    }
+    return deathYear - birthYear
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
